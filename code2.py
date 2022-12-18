d1df7208c8c238e91c3351ea66aee6c1b77731ae
anshchawla521 <anshchawla521@gmail.com>
2022-12-19 02:50:28 +0530

    Rfid scan + entry into Excel sheet

--- code2.py
+++ code2.py
@@ -1,30 +1,32 @@
 import os
 import numpy as np
 import cv2
-# import fingerprint_enhancer 
+# import fingerprint_enhancer
+
+path_to_dataset = "SOCOFing/updated"
+path_to_finger_image = "./SOCOFing/cam/cap_orginal_finger1_image4_grayscale.jpg"
 
 
-sample = cv2.imread( "./SOCOFing/cam/cap_orginal_finger1_image4_grayscale.jpg")
+sample = cv2.imread(path_to_finger_image)
 
-cv2.imshow("Wihout filter",sample)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.imshow("Wihout filter",sample)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
 # sample = cv2.imread( "./SOCOFing/Altered\Altered-Easy/1__M_Left_index_finger_Zcut.BMP")
 
 sample = cv2.cvtColor(sample, cv2.COLOR_BGR2GRAY)
-sample=abs(140-sample)
-sample=sample[90:310 , 170:480]
+sample = abs(140-sample)
+sample = sample[90:310, 170:480]
 kernel = np.array([[0, -1, 0],
-                   [-1, 6,-1],
+                   [-1, 6, -1],
                    [0, -1, 0]])
-    # kernel = np.array([[-1, -1, -1],
-    #                [-1, 9,-1],
-    #                [-1, -1, -1]])
- 
-sample = cv2.filter2D(src=sample, ddepth=-1, kernel=kernel)
+# kernel = np.array([[-1, -1, -1],
+#                [-1, 9,-1],
+#                [-1, -1, -1]])
 
+sample = cv2.filter2D(src=sample, ddepth=-1, kernel=kernel)
 
 
 cv2.imshow("Original", cv2.resize(sample, None, fx=1, fy=1))
@@ -33,50 +35,51 @@ cv2.destroyAllWindows()
 
 best_score = counter = 0
 filename = image = kp1 = kp2 = mp = None
-for file in os.listdir ("SOCOFing/updated"):
-     fingerprint_img = cv2.imread("./SOCOFing/updated/" + file)
-   #   fingerprint_img=abs(140-fingerprint_img)
-   #   fingerprint_img=fingerprint_img[90:310 , 170:480]
+for file in os.listdir(path_to_dataset):
+    fingerprint_img = cv2.imread(f'{path_to_dataset}/' + file)
+  #   fingerprint_img=abs(140-fingerprint_img)
+  #   fingerprint_img=fingerprint_img[90:310 , 170:480]
 
-     sift = ( cv2.SIFT_create() )
-     keypoints_1, des1 = sift.detectAndCompute(sample, None)
-     keypoints_2, des2 = sift.detectAndCompute(fingerprint_img, None)
-   #   index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    sift = (cv2.SIFT_create())
+    keypoints_1, des1 = sift.detectAndCompute(sample, None)
+    keypoints_2, des2 = sift.detectAndCompute(fingerprint_img, None)
+  #   index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 
-     matches = cv2.FlannBasedMatcher({"algorithm": 1, "trees": 10}, dict(checks=50)).knnMatch( des1, des2, k=2)
-   #   print(matches)
+    matches = cv2.FlannBasedMatcher(
+        {"algorithm": 1, "trees": 10}, dict(checks=50)).knnMatch(des1, des2, k=2)
+  #   print(matches)
 
-     match_points = []
-     for p, q in matches:
+    match_points = []
+    for p, q in matches:
         if p.distance < 0.1 * q.distance:   # it is lowe's ratio test checks if two distances are sufficiently different
             match_points.append(p)
 
-     keypoints = 0
-     if len(keypoints_1) <= len(keypoints_2):
+    keypoints = 0
+    if len(keypoints_1) <= len(keypoints_2):
         keypoints = len(keypoints_1)
-     else:
+    else:
         keypoints = len(keypoints_2)
 
-     if len(match_points) / keypoints * 100 > best_score:
+    if len(match_points) / keypoints * 100 > best_score:
         best_score = len(match_points) / keypoints * 100
         filename = file
         image = fingerprint_img
         kp1, kp2, mp = keypoints_1, keypoints_2, match_points
-      
-     if best_score >60:  # I have taken 60 % matching to be sufficient to identify if iamge is present or not in db
-         break
-    
 
-if best_score<40:
-   print("Not matched")
+    if best_score > 60:  # I have taken 60 % matching to be sufficient to identify if iamge is present or not in db
+        break
+
+
+if best_score < 40:
+    print("Not matched")
 else:
- print("Best match:  " + filename)
- print("Best score:  " + str(best_score))
- result = cv2.drawMatches(sample, kp1, image, kp2, mp, None)
- result = cv2.resize(result, None, fx=1, fy=1)
- cv2.imshow("Result", result)
- cv2.waitKey(0)
- cv2.destroyAllWindows()
+    print("Best match:  " + filename)
+    print("Best score:  " + str(best_score))
+    result = cv2.drawMatches(sample, kp1, image, kp2, mp, None)
+    result = cv2.resize(result, None, fx=1, fy=1)
+    cv2.imshow("Result", result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 # Now what we can do is take fingerprint of a person with different orientations and then store them in our db.
--- major_code.py
+++ major_code.py
@@ -1,12 +1,28 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-import time
+import datetime
+
+mapping = {"time":"A",
+            "uid":"B",
+            "name":"C",
+            "sid":"D",
+            "phone":"E",
+            "direction":"F",
+            "remarks":"G"}
+
 
 import cv2, picamera
 
 reader = SimpleMFRC522()
 
+GOINGOUT = True
+GOINGIN = False
 
+workbook = load_workbook(filename="attendance.xlsx")
+sheet = workbook["Sheet1"]
+
+
+direction = GOINGIN
 
 led = 7
 touch = 11
@@ -17,25 +33,67 @@ GPIO.output(led,GPIO.LOW)
 GPIO.setup(touch, GPIO.IN)
 
 camera = picamera.PiCamera()
+
+def getinfo(uid)-> dict:
+    return{"name" :"ansh",
+            "sid" :"19105031",
+            "uid" :"0",
+            "phone":"7696046760" }
+
+
+def match_finger(image)->bool:
+    return True
+
+
+
+
+
+
+
 try:
     #Reader.write("Ansh Chawla")
     id , text = reader.read()
     print(id)
     print(text)
+    row = workbook.max_row
 
+    person = getinfo(id)
+    
+    if(person == None):
+        raise IndexError("person not found invalid uid")
+    else:
+        #print(sheet[f'A{row}:F{row}'][0][1].value)
+        sheet[f'{mapping["time"]}{row}']= datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        sheet[f'{mapping["name"]}{row}']= person["name"]
+        sheet[f'{mapping["sid"]}{row}']= person["sid"]
+        sheet[f'{mapping["uid"]}{row}']= id
 
-    while True:
+        if direction == GOINGOUT:
+            sheet[f'{mapping["direction"]}{row}'] = "EXIT" # print name
+        elif direction == GOINGIN:
+            sheet[f'{mapping["direction"]}{row}'] = "ENTRY" 
+            
+    matched = False
+    if(direction == GOINGIN):
+        while not matched:
+            if not GPIO.input(touch):
+                GPIO.output(led,GPIO.HIGH)
+                image_path = "image.jpg"  #address and name of image
+                camera.capture(image_path) 
 
-        if not GPIO.input(touch):
-            GPIO.output(led,GPIO.HIGH)
-            image_path = "image.jpg"  #address and name of image
-            camera.capture(image_path) 
+                image = cv2.imread(image_path)
+                cv2.imshow("captured",image)
+                # call image_check
 
-            image = cv2.imread(image_path)
-            cv2.imshow("captured",image)
-            GPIO.output(led,GPIO.LOW)
-            if cv2.waitKey(0) == ord('s'):
-                break
+                GPIO.output(led,GPIO.LOW)
+                matched = match_finger(image)
+                if cv2.waitKey(0) == ord('s'):
+                    break
+            
+                
+    else:
+        # open the gate
+        pass
 
 finally:
     GPIO.cleanup()
